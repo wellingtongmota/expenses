cd4e7f7d5c4872de273e94124993d59dbffede23
Wellington Galvão <wellingtongalvao96@gmail.com>
2024-12-16 10:06:57 -0300

    build: correção de fontes do site

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next"
-import localFont from "next/font/local"
 import "./globals.css"
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900"
-})
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900"
-})
+import { inter } from "@/fonts"
+import { cn } from "@/lib/utils"
 
 export const metadata: Metadata = {
   title: "Expenses",
@@ -24,10 +14,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="pt-Br">
+      <body className={cn("min-h-[100dvh] antialiased", inter.className)}>
         {children}
       </body>
     </html>
