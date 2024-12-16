f8d3f7b088134d9ca31fa1e9401e92920936667f
Wellington Galvão <wellingtongalvao96@gmail.com>
2024-12-16 09:44:34 -0300

    build: instalação e formatação de código com prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <div>
       <Button variant="outline">Hello world</Button>
     </div>
-  );
+  )
 }
