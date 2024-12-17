cd6fcca0d9d5e8d2d48410e666e20362ebb7efa8
Wellington Galvão <wellingtongalvao96@gmail.com>
2024-12-17 09:40:02 -0300

    build: instalação Prisma ORM e criação de tabelas

--- auth.ts
+++ auth.ts
@@ -1,8 +1,11 @@
 import NextAuth from "next-auth"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "@/prisma"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
   secret: process.env.AUTH_SECRET,
+  adapter: PrismaAdapter(prisma),
+  providers: [],
   pages: {
     signIn: "/login",
     signOut: "/login",
