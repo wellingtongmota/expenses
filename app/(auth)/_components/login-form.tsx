6d670c04ac1e572f151641bda55ae4aee231bf42
Wellington Galvão <wellingtongalvao96@gmail.com>
2024-12-17 14:54:16 -0300

    fix: correção em LoginForm

--- app/(auth)/_components/login-form.tsx
+++ app/(auth)/_components/login-form.tsx
@@ -1,63 +1,135 @@
-import { cn } from "@/lib/utils"
+"use client"
+
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import Link from "next/link"
-import { signIn } from "@/auth"
+import { signIn } from "next-auth/react"
+import { useForm } from "react-hook-form"
+import { loginSchema } from "@/schemas/authentication"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { z } from "zod"
+import { useToast } from "@/hooks/use-toast"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "@/components/ui/form"
+import { LoaderCircle } from "lucide-react"
+import { useRouter } from "next/navigation"
 
-export function LoginForm({
-  className,
-  ...props
-}: React.ComponentPropsWithoutRef<"form">) {
+export function LoginForm() {
+  const { toast } = useToast()
+  const router = useRouter()
+
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: ""
+    }
+  })
+
+  const onSubmit = async (data: z.infer<typeof loginSchema>) => {
+    const result = await signIn("credentials", {
+      email: data.email,
+      password: data.password,
+      redirect: false // Evita redirecionamento automático
+    })
+
+    if (result?.error) {
+      toast({
+        title: "Erro ao entrar",
+        description: "Credenciais inválidas. Verifique seu email e senha.",
+        variant: "destructive"
+      })
+    } else {
+      toast({
+        title: "Login realizado com sucesso",
+        description: "Bem-vindo de volta!",
+        variant: "default"
+      })
+      router.push("/app")
+    }
+  }
+
   return (
-    <form
-      className={cn("flex flex-col gap-6", className)}
-      {...props}
-      action={async (formData) => {
-        "use server"
-
-        await signIn("credentials", formData)
-      }}
-    >
-      <div className="flex flex-col items-center gap-2 text-center">
-        <h1 className="text-2xl font-bold">Faça login na sua conta</h1>
-        <p className="text-balance text-sm text-muted-foreground">
-          Insira seu e-mail abaixo para acessar sua conta
-        </p>
-      </div>
-      <div className="grid gap-6">
-        <div className="grid gap-2">
-          <Label htmlFor="email">E-mail</Label>
-          <Input
-            id="email"
+    <Form {...form}>
+      <form
+        className="flex flex-col gap-6"
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
+        <div className="flex flex-col items-center gap-2 text-center">
+          <h1 className="text-2xl font-bold">Faça login na sua conta</h1>
+          <p className="text-balance text-sm text-muted-foreground">
+            Insira seu e-mail abaixo para acessar sua conta
+          </p>
+        </div>
+        <div className="grid gap-6">
+          <FormField
+            control={form.control}
             name="email"
-            type="email"
-            placeholder="m@example.com"
-            required
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>E-mail</FormLabel>
+                  <FormControl>
+                    <Input id="email" {...field} placeholder="m@examplo.com" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )
+            }}
           />
+
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <div className="flex items-center">
+                    <FormLabel>Senha</FormLabel>
+                    <a
+                      href="#"
+                      className="ml-auto text-sm underline-offset-4 hover:underline"
+                    >
+                      Esqueceu sua senha?
+                    </a>
+                  </div>
+                  <FormControl>
+                    <Input id="password" type="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )
+            }}
+          />
+
+          <Button
+            type="submit"
+            className="w-full"
+            disabled={form.formState.isSubmitting}
+          >
+            {form.formState.isSubmitting ? (
+              <>
+                <LoaderCircle className="mr-2 h-4 w-4 animate-spin" />
+                Entrando
+              </>
+            ) : (
+              "Entrar"
+            )}
+          </Button>
         </div>
-        <div className="grid gap-2">
-          <div className="flex items-center">
-            <Label htmlFor="password">Senha</Label>
-            <a
-              href="#"
-              className="ml-auto text-sm underline-offset-4 hover:underline"
-            >
-              Esqueceu sua senha?
-            </a>
-          </div>
-          <Input id="password" name="password" type="password" required />
+        <div className="text-center text-sm">
+          Não tem uma conta?{" "}
+          <Link href="/register" className="underline underline-offset-4">
+            Cadastre-se
+          </Link>
         </div>
-        <Button type="submit" className="w-full">
-          Entrar
-        </Button>
-      </div>
-      <div className="text-center text-sm">
-        Não tem uma conta?{" "}
-        <Link href="/register" className="underline underline-offset-4">
-          Cadastre-se
-        </Link>
-      </div>
-    </form>
+      </form>
+    </Form>
   )
 }
--- schemas/authentication.ts
+++ schemas/authentication.ts
@@ -1,6 +1,13 @@
 import { z } from "zod"
 
 export const loginSchema = z.object({
-  email: z.string().min(1, { message: "Insira o e-mail" }),
-  password: z.string().min(1, { message: "Insira a senha" })
+  email: z
+    .string({ required_error: "E-mail é obrigatório" })
+    .min(1, "E-mail é obrigatório")
+    .email("E-mail inválido"),
+  password: z
+    .string({ required_error: "Senha é obrigatória" })
+    .min(1, "Senha é obrigatória")
+    .min(4, "A senha deve ter mais de 4 caracteres")
+    .max(32, "A senha deve ter menos de 32 caracteres")
 })
