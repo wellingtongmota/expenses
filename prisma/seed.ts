0fb23364ffee7b4496fa67ddc718b4fb2bf42eca
Wellington Galvão <wellingtongalvao96@gmail.com>
2024-12-22 12:18:56 -0300

    build: alteração no seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -7,57 +7,57 @@ async function main() {
   const tags = [
     {
       name: "Alimentação",
-      userId: "cm4t4i9rz0000rf44ggn90afh",
+      userId: "user-id-here",
       color: "bg-red-500"
     },
     {
       name: "Moradia",
-      userId: "cm4t4i9rz0000rf44ggn90afh",
+      userId: "user-id-here",
       color: "bg-yellow-400"
     },
     {
       name: "Transporte",
-      userId: "cm4t4i9rz0000rf44ggn90afh",
+      userId: "user-id-here",
       color: "bg-cyan-300"
     },
     {
       name: "Saúde",
-      userId: "cm4t4i9rz0000rf44ggn90afh",
+      userId: "user-id-here",
       color: "bg-pink-300"
     },
     {
       name: "Educação",
-      userId: "cm4t4i9rz0000rf44ggn90afh",
+      userId: "user-id-here",
       color: "bg-fuchsia-500"
     },
     {
       name: "Lazer",
-      userId: "cm4t4i9rz0000rf44ggn90afh",
+      userId: "user-id-here",
       color: "bg-teal-400"
     },
     {
       name: "Financeiro",
-      userId: "cm4t4i9rz0000rf44ggn90afh",
+      userId: "user-id-here",
       color: "bg-red-400"
     },
     {
       name: "Roupas e Acessórios",
-      userId: "cm4t4i9rz0000rf44ggn90afh",
+      userId: "user-id-here",
       color: "bg-indigo-600"
     },
     {
       name: "Tecnologia",
-      userId: "cm4t4i9rz0000rf44ggn90afh",
+      userId: "user-id-here",
       color: "bg-indigo-800"
     },
     {
       name: "Presentes e Doações",
-      userId: "cm4t4i9rz0000rf44ggn90afh",
+      userId: "user-id-here",
       color: "bg-teal-600"
     },
     {
       name: "Outros",
-      userId: "cm4t4i9rz0000rf44ggn90afh",
+      userId: "user-id-here",
       color: "bg-orange-400"
     }
   ]
